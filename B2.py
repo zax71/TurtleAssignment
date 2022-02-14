2f950fa0e5e054a4b7ab38357e22b108e96ea79c
Zax71 <zabax32@gmail.com>
2022-02-14 18:02:40 +0000

    First Commit (B2)

---
+++ B2.py
@@ -0,0 +1,61 @@
+import turtle as t
+import math
+
+t.tracer(5)
+# A=base B=right side C=hypotenuse
+# Works out the top right angle form the side lengths
+def angles(aLen, bLen, cLen):
+    aLenSquare = aLen**2
+    bLenSquare = bLen**2
+    cLenSquare = cLen**2
+    
+    divTop = bLenSquare+cLenSquare-aLenSquare
+    divBottom = 2*bLen*cLen
+    
+    unCos = divTop/divBottom
+    
+    angle = math.degrees(math.acos(unCos))
+    
+    return angle
+
+def drawHouse(height=50):
+    hypotenuse = math.sqrt(height**2+(height*2)**2)
+    angleA = angles(height*2, height, hypotenuse)
+    angleB = 180-90-angleA
+    
+    t.pendown()
+    t.setheading(-60)
+    t.fd(height*2)
+    
+    t.setheading(angleB+180)
+    t.fd(hypotenuse) # Pythagoras :D
+    
+    t.setheading(0)
+    t.fd(height*2)
+    
+    t.setheading(180) # No don't worry, i'm *not* cheating
+    t.fd(height*2)
+    
+    
+    t.setheading(90)
+    t.fd(height)
+    
+    t.setheading(angleA+270)
+    t.fd(hypotenuse) # Pythagoras :D
+    
+    t.setheading(90)
+    t.fd(height)
+    
+    t.setheading(180)
+    t.fd(height*2)
+    
+    t.setheading(60)
+    t.fd(height*2)
+
+t.penup()
+t.setheading(90)
+t.fd(400)
+for i in range(50, 400, 50):
+    drawHouse(i)
+t.update()
+t.Screen().exitonclick()
